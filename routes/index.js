a11d0018bcd71697d6d166ec0c343275e3999aad
Aditya kumar <Adityakumar14022002@gmail.com>
2023-07-31 00:28:55 +0530

    add update feature in todo list

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ router.get("/", function (req, res, next) {
   res.render("index", { title: "  TODO APPLICATION" });
 });
 
+
+// ......................login route..........................
 router.get("/login", function (req, res, next) {
   res.render("login", { title: "Login Page" });
 });
@@ -19,14 +21,15 @@ router.post("/login", async function (req, res, next) {
       return res.send(`email not found. <a href="/login">login again</a>`);
     }
     if (user.password !== password) {
-      return res.send(`Incorrect Password. <a href="/signin">signin</a>`);
+      return res.send(`Incorrect Password. <a href="/login">signin</a>`);
     }
     res.redirect("/profile");
   } catch (error) {
     console.log(err);
   }
 });
-
+// .....................end....................................
+// .......................signup route.................................
 router.get("/signup", function (req, res, next) {
   res.render("signup", { title: "Sign Up Page" });
 });
@@ -39,11 +42,13 @@ router.post("/signup", async function (req, res, next) {
     console.log(error);
   }
 });
-
+// .....................end....................................
+// .....................profile route....................................
 router.get("/profile", async function (req, res, next) {
   const posts = await Data.find();
   res.render("profile", { posts });
 });
+// .....................createtask route ....................................
 
 router.get("/createtask", function (req, res, next) {
   res.render("createtask", { title: "fiil your todo application" });
@@ -58,6 +63,9 @@ router.post("/createtask", async function (req, res, next) {
     console.log(error);
   }
 });
+// .....................end....................................
+// .....................delete route....................................
+
 router.get("/delete/:id", async function (req, res, next) {
   try {
     await Data.findByIdAndDelete(req.params.id);
@@ -66,5 +74,25 @@ router.get("/delete/:id", async function (req, res, next) {
     res.send(error);
   }
 });
+
+
+router.get("/update/:id", async function(req, res, next) {
+   try {
+    const todata = await Data.findById(req.params.id);
+    res.render("update",{post:todata})
+   } catch (error) {
+    console.log(error)
+   }
+});
+
+router.post("/update/:id", async function(req, res, next) {
+  try {
+    await Data.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect("/profile");
+} catch (error) {
+    res.send(error);
+}
+});
+
 
 module.exports = router;

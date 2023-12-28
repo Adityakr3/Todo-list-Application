f6f0744675c19cb51730049c4d8202cdb68eb47c
Aditya kumar <Adityakumar14022002@gmail.com>
2023-12-28 16:53:38 +0530

    db fixed

--- model/db.js
+++ model/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
 mongoose
-    .connect("mongodb+srv://Adi123:d6bSX5QMW1UE0vdP@cluster0.m1kllof.mongodb.net/?retryWrites=true&w=majority")
+    .connect("mongodb+srv://adityac22f:eXLb2LMhcgozHrAA@cluster0.ndt99w0.mongodb.net/?retryWrites=true&w=majority")
     .then(() => console.log("db connected!"))
     .catch((err) => console.log(err));

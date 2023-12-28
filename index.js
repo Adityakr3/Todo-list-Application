841cfb061bc169a4c43c46b7cda36ddd84a437f7
Aditya kumar <Adityakumar14022002@gmail.com>
2023-12-28 14:09:04 +0530

    " x=> o"

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('./model/db.js');
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/users.js');
 
 var app = express();
 

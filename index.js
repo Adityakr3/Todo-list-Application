1671f9b9e5487f9301fb37b82dc5c2cc277159a7
Aditya kumar <Adityakumar14022002@gmail.com>
2023-12-28 14:54:27 +0530

    " X => O "

--- index.js
+++ index.js
@@ -4,14 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('./model/db.js');
-
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users.js');
 
 var app = express();
 
 // view engine setup
-app.use(express.static("public"));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
